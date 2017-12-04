8e73fdff7c8b79b9d285cb7feac6cb4c9b78d0c2
victor.wu <victor.wu@vatics.com>
2017-12-04 17:03:05 +0800

    Add SPI_IOC_DATA_READ and SPI_IOC_DATA_WRITE to spidev_test.c

--- spidev/spidev_test.c
+++ spidev/spidev_test.c
@@ -7,20 +7,24 @@
 #include <sys/ioctl.h>
 #include <linux/limits.h>
 #include <asm-generic/errno-base.h>
+#include <linux/spi/spidev.h>
+#include <linux/version.h>
 
 #define DEFAULT_SPIDEV "/dev/spidev0.1"
 
-#define SPI_IOC_RD_MODE			0x80016b01
-#define SPI_IOC_RD_LSB_FIRST		0x80016b02
-#define SPI_IOC_RD_BITS_PER_WORD	0x80016b03
-#define SPI_IOC_RD_MAX_SPEED_HZ		0x80046b04
-#define SPI_IOC_WR_MODE			0x40016b01
-#define SPI_IOC_WR_LSB_FIRST		0x40016b02
-#define SPI_IOC_WR_BITS_PER_WORD	0x40016b03
-#define SPI_IOC_WR_MAX_SPEED_HZ		0x40046b04
-#define SPI_IOC_EEPROM_READ		0xc0046b05
-#define SPI_IOC_FLASHID_READ		0xc0046b06
-#define SPI_IOC_EEPROM_WRITE		0xc0046b07
+#if LINUX_VERSION_CODE < KERNEL_VERSION(4,9,30)
+#define KERNEL_OLD 1
+#else
+#define KERNEL_OLD 0
+#endif
+
+#if KERNEL_OLD
+#define SPI_IOC_EEPROM_READ		_IOWR(SPI_IOC_MAGIC, 5, __u32)
+#define SPI_IOC_FLASHID_READ		_IOWR(SPI_IOC_MAGIC, 6, __u32)
+#define SPI_IOC_EEPROM_WRITE		_IOWR(SPI_IOC_MAGIC, 7, __u32)
+#define SPI_IOC_DATA_READ		_IOWR(SPI_IOC_MAGIC, 8, __u32)
+#define SPI_IOC_DATA_WRITE		_IOW(SPI_IOC_MAGIC, 9, __u32)
+#endif
 
 enum SPIDEV_MENU_ITEM {
 	SPIDEV_MENU_ITEM_RD_MODE = 0x0,
@@ -31,9 +35,13 @@ enum SPIDEV_MENU_ITEM {
 	SPIDEV_MENU_ITEM_WR_LSB_FIRST,
 	SPIDEV_MENU_ITEM_WR_BITS_PER_WORD,
 	SPIDEV_MENU_ITEM_WR_MAX_SPEED_HZ,
+#if KERNEL_OLD
 	SPIDEV_MENU_ITEM_EEPROM_READ,
 	SPIDEV_MENU_ITEM_FLASHID_READ,
 	SPIDEV_MENU_ITEM_EEPROM_WRITE,
+	SPIDEV_MENU_ITEM_DATA_READ,
+	SPIDEV_MENU_ITEM_DATA_WRITE,
+#endif
 	SPIDEV_MENU_ITEM_EXIT,
 	SPIDEV_MENU_ITEM_NUMBER
 };
@@ -46,6 +54,10 @@ enum SPIDEV_MENU_ITEM {
 	printf("\t-h, --help\tshow this help\n\n");\
 	exit(EXIT_SUCCESS);\
 }
+
+#define UINT16_TO_UCHARS(v, x)\
+        *((unsigned char *)x + 0)  = v & 0xff;\
+        *((unsigned char *)x + 1) = (v >> 8) & 0xff
 
 struct option long_options[] = {
 	{"name", required_argument, 0, 'n'},
@@ -66,9 +78,13 @@ static unsigned int show_menu(void)
 		printf("%d) write lsb first\n", SPIDEV_MENU_ITEM_WR_LSB_FIRST);
 		printf("%d) write bits per word\n", SPIDEV_MENU_ITEM_WR_BITS_PER_WORD);
 		printf("%d) write max speed (Hz)\n", SPIDEV_MENU_ITEM_WR_MAX_SPEED_HZ);
+#if KERNEL_OLD
 		printf("%d) eeprom read\n", SPIDEV_MENU_ITEM_EEPROM_READ);
 		printf("%d) read flash id\n", SPIDEV_MENU_ITEM_FLASHID_READ);
 		printf("%d) eeprom write\n", SPIDEV_MENU_ITEM_EEPROM_WRITE);
+		printf("%d) data read\n", SPIDEV_MENU_ITEM_DATA_READ);
+		printf("%d) data write\n", SPIDEV_MENU_ITEM_DATA_WRITE);
+#endif
 		printf("%d) exit\n", SPIDEV_MENU_ITEM_EXIT);
 		printf("select: ");
 		scanf("%u", &select);
@@ -79,6 +95,24 @@ static unsigned int show_menu(void)
 	}
 
 	return select;
+}
+
+int spidev_transfer(int fd, unsigned char *txbuf, unsigned char *rxbuf,int bytes)
+{
+	int ret;
+	struct spi_ioc_transfer tr = {
+		.tx_buf = (unsigned long)txbuf,
+		.rx_buf = (unsigned long)rxbuf,
+		.len = bytes,
+	};
+
+	ret = ioctl(fd, SPI_IOC_MESSAGE(1), &tr);
+	if (ret < 0)
+	{
+		printf("can't send spi message");
+		return -1;
+	}
+	return ret;
 }
 
 static int spidev_read_mode(int fd, unsigned char *mode)
@@ -169,11 +203,13 @@ static int spidev_write_max_speed_hz(int fd)
 	return ioctl(fd, SPI_IOC_WR_MAX_SPEED_HZ, &hz);
 }
 
+#if KERNEL_OLD
+#if 0
 static int spidev_eeprom_read(int fd, unsigned char *rx)
 {
 	return ioctl(fd, SPI_IOC_EEPROM_READ, rx);
 }
-
+#endif
 static int spidev_flashid_read(int fd, unsigned char *id)
 {
 	return ioctl(fd, SPI_IOC_FLASHID_READ, id);
@@ -183,6 +219,26 @@ static int spidev_eeprom_write(int fd, unsigned char *tx)
 {
 	return ioctl(fd, SPI_IOC_EEPROM_WRITE, tx);
 }
+
+static int spidev_data_read(int fd, unsigned char *buf, unsigned int txlen, unsigned int rxlen)
+{
+	unsigned char buffer[5];
+
+	UINT16_TO_UCHARS(txlen, buffer);
+	UINT16_TO_UCHARS(rxlen, buffer + 2);
+
+	return ioctl(fd, SPI_IOC_DATA_READ, buffer);
+}
+
+static int spidev_data_write(int fd, unsigned char *tx, unsigned int txlen)
+{
+	unsigned char buffer[3];
+
+	UINT16_TO_UCHARS(txlen, buffer);
+
+	return ioctl(fd, SPI_IOC_DATA_WRITE, buffer);
+}
+#endif
 
 int main(int argc, char *const *argv)
 {
@@ -256,12 +312,7 @@ int main(int argc, char *const *argv)
 		case SPIDEV_MENU_ITEM_WR_MAX_SPEED_HZ:
 			ret = spidev_write_max_speed_hz(fd);
 			break;
-		case SPIDEV_MENU_ITEM_EEPROM_READ:
-			buffer[0] = 0x5;
-			ret = spidev_eeprom_read(fd, buffer);
-			if (!ret)
-				printf("##### sr: %#x #####\n", buffer[0]);
-			break;
+#if KERNEL_OLD
 		case SPIDEV_MENU_ITEM_FLASHID_READ:
 			buffer[0] = 0x9f;
 			ret = spidev_flashid_read(fd, buffer);
@@ -273,6 +324,23 @@ int main(int argc, char *const *argv)
 			buffer[0] = 0x6;
 			ret = spidev_eeprom_write(fd, buffer);
 			break;
+		case SPIDEV_MENU_ITEM_DATA_READ:
+			buffer[4] = 0x9f;
+			ret = spidev_data_read(fd, buffer, 1, 3);
+			if (!ret)
+				printf("##### id: %#x %#x %#x #####\n", buffer[0], buffer[1], buffer[2]);
+			break;
+		case SPIDEV_MENU_ITEM_DATA_WRITE:
+			buffer[2] = 0x6;
+			ret = spidev_data_write(fd, buffer, 1);
+			if (!ret) {
+				buffer[4] = 0x5;
+				spidev_data_read(fd, buffer, 1, 1);
+				if (!ret)
+					printf("##### sr: %#x #####\n", buffer[0]);
+			}
+			break;
+#endif
 		case SPIDEV_MENU_ITEM_EXIT:
 			goto exit;
 			break;
