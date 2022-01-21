43c111dfc2ac68a4a26e2a4d00fc0f67b5cfa4cc
Victor Wu(Wu, Jheng-Jhong) <victor_wu@bizlinktech.com>
2022-01-21 16:25:01 +0800

    Add a MTD test script

---
+++ mtd/mtd_tester.sh
@@ -0,0 +1,87 @@
+#!/bin/sh
+#
+# Test MTD read/write/erase.
+# This script must run with mtd_debug, please ensure mtd_debug exists.
+#
+# author: Victor Wu (victor_wu@bizlinktech.com)
+# version: 20220121
+
+### Test functions ###
+
+show_help()
+{
+	echo -e "\nTest MTD read/write/erase"
+	echo -e "\nUsage: $(basename $0) <-d device> [-l loop_time] [-h]"
+	echo -e "\t-d\tmtd device"
+	echo -e "\t-l\tloop time"
+	echo -e "\t-h\tshow this help\n"
+}
+
+test_mtd()
+{
+	local mtd_size=$(mtd_debug info $1 | grep 'mtd\.size' | awk '{ print $3 }')
+	local mtd_erasesize=$(mtd_debug info $1 | grep 'mtd\.erasesize' | awk '{ print $3 }')
+	local max_test_size=$((mtd_erasesize * 10))
+	local offset=0
+
+	for i in $(seq $2); do
+		while [ ${mtd_size} -gt 0 ]; do
+			if [ ${max_test_size} -le ${mtd_size} ]; then
+				local test_size=${max_test_size}
+			else
+				local test_size=${mtd_size}
+			fi
+
+			mtd_debug read $1 ${offset} ${test_size} /tmp/$(basename $1)
+			mtd_debug erase $1 ${offset} ${test_size}
+			mtd_debug write $1 ${offset} ${test_size} /tmp/$(basename $1)
+			mtd_debug read $1 ${offset} ${test_size} /tmp/$(basename $1)-2
+			cmp /tmp/$(basename $1) /tmp/$(basename $1)-2
+			if [ $? -ne 0 ]; then
+				echo "$1 rw test failed - offset: ${offset} rw size: ${test_size}"
+				return 1
+			fi
+
+			offset=$((offset + test_size))
+			mtd_size=$((mtd_size - test_size))
+		done
+	done
+
+	rm -f /tmp/$(basename $1) > /dev/null
+	rm -f /tmp/$(basename $1)-2 > /dev/null
+}
+
+### Main program ###
+
+loop=1
+
+options="d:l:h"
+while getopts ${options} opt
+do
+	case ${opt} in
+	d)
+		dev=${OPTARG};;
+	l)
+		loop=${OPTARG}
+		if [ ${loop} -lt 1 ]; then
+			echo "Loop time must >= 1"
+			exit 1
+		fi
+		;;
+	*)
+		show_help $0
+		exit 0;;
+	esac
+done
+
+if [ -n "${dev}" ]; then
+	test_mtd ${dev} ${loop}
+	if [ $? -eq 0 ]; then
+		echo "Test OK!"
+	else
+		echo "Test failed!"
+	fi
+else
+	show_help
+	exit 1
+fi
