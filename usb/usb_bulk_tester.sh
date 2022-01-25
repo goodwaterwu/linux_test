4061f3f905b6e15f500e752e178647e83505b935
Victor Wu(Wu, Jheng-Jhong) <victor_wu@bizlinktech.com>
2022-01-25 14:51:55 +0800

    Modify USB bulk test

--- usb/usb_bulk_tester.sh
+++ usb/usb_bulk_tester.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 #
 # Test a USB bulk device like USB stick, disk, SSD.
+# This test script must run with pattern_generator, please ensure pattern_generator exists.
 #
-# author: Victor Wu (goodwater.wu@gmail.com)
+# author: Victor Wu (victor_wu@bizlinktech.com)
 # version: 20220124
 
 ### Functions ###
@@ -40,25 +41,37 @@ do
 	esac
 done
 
-if [ ! -f ${device} ] || [ ${loop} -lt 1 ] || [ ! -f ${src} ]; then
+if [ ! -e ${device} ] || [ ${loop} -lt 1 ]; then
 	show_help $0
+	exit 1
 fi
 
 mkdir -p ${test_dir}
 mount ${device} ${test_dir} 2>/dev/null
+echo -n "Generate a pattern file..."
+rm -f ${src}
+pattern_generator -f ${src} -s 104857600
+echo "OK"
+
 for i in $(seq ${loop}); do
 	src_size=$(stat -c %s ${src})
 
-	echo "loop...${i}"
-	dd if=${src} of=${test_dir}/$(basename ${src}) conv=sync,noerror
-	dd if=${test_dir}/$(basename ${src}) of=${src}.bak conv=sync,noerror
-	cmp ${src} ${src}.bak
+	echo "Loop...$i"
+	dd if=${src} of=${test_dir}/$(basename ${src}).w conv=sync,noerror
+	cmp ${src} ${test_dir}/$(basename ${src}).w
 	if [ $? -ne 0 ]; then
-		echo "Test failed!"
+		echo "Write test failed!"
 		exit 1
 	fi
 
-	rm -f ${test_dir}/$(basename ${src})
+	dd if=${test_dir}/$(basename ${src}).w of=${src}.r conv=sync,noerror
+	cmp ${src} ${src}.r
+	if [ $? -ne 0 ]; then
+		echo "Read test failed!"
+		exit 1
+	fi
+
+	rm -f ${src}.r ${test_dir}/$(basename ${src}).w
 done
 
 umount ${test_dir}
